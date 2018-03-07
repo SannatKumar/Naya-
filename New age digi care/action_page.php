db3ea82f5fb2a967c8d9ce7f9001d9ac5e360898
Raj <rz_kumr@yahoo.com>
2018-03-07 21:10:01 +0200

    Updated version

---
+++ New age digi care/action_page.php
@@ -0,0 +1,35 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "newAgeDigiDB";
+
+	try {
+    		$connect = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    		// set the PDO error mode to exception
+    		$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    		echo "Connected successfully";
+    	}
+	catch(PDOException $e) {
+    		echo "Connection failed: " . $e->getMessage();
+    	}
+	try {
+		// Sanitize user input here
+
+		// Check if unique here
+
+
+		$sql =  "INSERT INTO users (username, email)
+							VALUES ('$_POST[username]', '$_POST[email]')";
+    		// use exec() because no results are returned
+    		$connect->exec($sql);
+    		echo "New record created successfully";
+	}
+	catch(PDOException $e){
+		echo "Database Error: " . $e->getMessage();
+	}
+
+	$connect = null;
+
+	header('Location: index.html');
+?>
